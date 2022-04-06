80277765e6e13e86814f9dcaa87628637fa59710
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-04-06 11:35:59 +0300

    tyhjä person_view + urls

--- varasto/views.py
+++ varasto/views.py
@@ -10,3 +10,9 @@ def index(request):
     return render(request, 'varasto/login.html')
 
 
+def person_view(request):
+    
+
+    return render(request, 'varasto/person.html')
+
+
--- varastoapp/urls.py
+++ varastoapp/urls.py
@@ -21,4 +21,5 @@ from varasto import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    path('person/', views.person_view, name='person'),
 ]
