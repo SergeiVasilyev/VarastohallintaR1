20f7e9e4a3dc18c151f4bcc11c1fb0d5fd36cb03
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-04-07 13:13:39 +0300

    models

--- varasto/models.py
+++ varasto/models.py
@@ -7,15 +7,15 @@ from django.utils.translation import gettext as _
 class CustomUser(AbstractUser):
     ROLE = [
         ("student", _("Student")), # Opiskelia ei voi kirjautua ja tehdä mitään palvelussa
-        ("employee", _("Employee")), # Varasto työntekiä ei voi lisätä, muokata ja posta tavaraa
-        ("management", _("Management")), # Taloushallinto ei voi lainata tavara, antaa oikeuksia . Voi katsoa tapahtuma
-
+        ("student_ext", _("Student_ext")), # Okeus, joka antaa opiskelioille päästä Varasto palveluun ja hän voi alkaa ja päättää vuokra tapahtuma.
+        ("employee", _("Employee")), # Varasto työntekiä ei voi lisätä, muokata ja posta tavaraa, voi alkaa ja päättää vuokra tapahtuma, antaa oikeuksia opiskelioille
+        ("management", _("Management")), # Taloushallinto ei voi lainata tavara, mutta voi antaa oikeuksia. Voi katsoa tapahtumat.
     ]
 
     phone = models.CharField(max_length=15)
     code = models.CharField(max_length=10)
     photo = models.CharField(max_length=255) # Käytetään tässä URL
-    role = models.CharField(choices=ROLE)
+    role = models.CharField(max_length=255, choices=ROLE)
 
 
 
