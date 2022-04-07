096ad35a0347c54ce352f6371698015c5aeb7ebf
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-04-07 12:05:22 +0300

    new model

--- varasto/models.py
+++ varasto/models.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 
 class CustomUser(AbstractUser):
-    testf = models.CharField(max_length=100)
+    ROLE = [
+        ("student", _("Student")), # Opiskelia ei voi kirjautua ja tehdä mitään palvelussa
+        ("employee", _("Employee")), # Varasto työntekiä ei voi lisätä, muokata ja posta tavaraa
+        ("management", _("Management")), # Taloushallinto ei voi lainata tavara, antaa oikeuksia . Voi katsoa tapahtuma
+
+    ]
+
+    phone = models.CharField(max_length=15)
+    code = models.CharField(max_length=10)
+    photo = models.CharField(max_length=255) # Käytetään tässä URL
+    role = models.CharField(choices=ROLE)
+
+
+
     # REQUIRED_FIELDS = ['testf']
 
     def __str__(self):
