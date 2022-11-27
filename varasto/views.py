bebe2c0bdc4504551813e6b6f91d739d25493683
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-27 20:47:52 +0200

    inventory imported in views

--- varasto/views.py
+++ varasto/views.py
@@ -28,7 +28,7 @@ from django.db.models import Count
 from django.db.models import Min, Max
 from .test_views import test
 
-from .anna__views import report, new_event_goods, product_report
+from .anna__views import report, new_event_goods, product_report, inventory
 
 from .capture_picture import VideoCamera
 from django.db.models import Q
